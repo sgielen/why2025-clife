792d989587bfb7c8fff9b35f07d0aa7ece28887a
HP van Braam <hp@tmm.cx>
2025-07-11 15:40:31 +0200

    Add licensing information

--- main/elf_symbols.h
+++ main/elf_symbols.h
@@ -4,37 +4,16 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma once
 
-#include "khash.h"
-#include "pathfuncs.h"
-
-#include <stddef.h>
-#include <stdint.h>
-
-#include <sys/types.h>
-
-KHASH_MAP_INIT_STR(devtable, void *);
-extern khash_t(devtable) * device_table;
-
-typedef struct device_s {
-    int (*_open)(void *dev, path_t *path, int flags, mode_t mode);
-    int (*_close)(void *dev, int fd);
-    ssize_t (*_write)(void *dev, int fd, void const *buf, size_t count);
-    ssize_t (*_read)(void *dev, int fd, void const *buf, size_t count);
-    ssize_t (*_lseek)(void *dev, int fd, off_t offset, int whence);
-} device_t;
-
-void      device_init();
-int       device_register(char const *name, device_t *device);
-device_t *device_get(char const *name);
+extern uintptr_t elf_find_sym(const char *sym_name);
