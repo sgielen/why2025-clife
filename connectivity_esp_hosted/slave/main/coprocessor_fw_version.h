50d1c26b358558c47c9eec9d17c3341fa0031eff
Jonas Meeuws <jonas.meeuws@gmail.com>
2025-07-30 18:40:10 +0200

    connectivity_esp_hosted: Checkout from test-firmware/slave_c6 v1.2.0.

---
+++ connectivity_esp_hosted/slave/main/coprocessor_fw_version.h
@@ -0,0 +1,8 @@
+/* this is a generated file - do not modify
+ * generated from idf_component.yml */
+#ifndef __COPROCESSOR_FW_VERSION_H__
+#define __COPROCESSOR_FW_VERSION_H__
+#define PROJECT_VERSION_MAJOR_1 2
+#define PROJECT_VERSION_MINOR_1 0
+#define PROJECT_VERSION_PATCH_1 17
+#endif
